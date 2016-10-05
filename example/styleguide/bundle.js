44f7f5dcae07af393cc82b2936312e6f92d24da1
Charles Hacskaylo <charlesh88@gmail.com>
2016-10-05 15:10:41 -0700

    [Frontend] WIP: Style Guide
    Fixes #1233
    WIP: bundle, model provider, stubbed pages

---
+++ example/styleguide/bundle.js
@@ -0,0 +1,71 @@
+define([
+    "./src/ExampleStyleGuideModelProvider",
+    'legacyRegistry'
+], function (
+    ExampleStyleGuideModelProvider,
+    legacyRegistry
+) {
+    legacyRegistry.register("example/styleguide", {
+        "name": "Open MCT Style Guide",
+        "description": "Examples and documentation illustrating UI styles in use in Open MCT.",
+        "extensions":
+        {
+            "types": [
+                {
+                    "key": "styleguide.intro",
+                    "name": "Introduction",
+                    "cssclass": "icon-page",
+                    "description": "Introduction and overview to the style guide."
+                },
+                {
+                    "key": "styleguide.controls",
+                    "name": "Controls",
+                    "cssclass": "icon-page",
+                    "description": "Open MCT Controls"
+                }
+            ],
+            "views": [
+                {
+                    "key": "styleguide.intro",
+                    "type": "styleguide.intro",
+                    "cssclass": "icon-page",
+                    "name": "Introduction",
+                    "templateUrl": "templates/intro.html",
+                    "editable": false
+                },
+                {
+                    "key": "styleguide.controls",
+                    "type": "styleguide.controls",
+                    "cssclass": "icon-page",
+                    "name": "Controls",
+                    "templateUrl": "templates/controls.html",
+                    "editable": false
+                }
+            ],
+            "roots": [
+                {
+                    "id": "styleguide:folder",
+                    "priority" : "preferred",
+                    "model": {
+                        "type": "folder",
+                        "name": "Style Guide",
+                        "composition": [
+                            "page0",
+                            "page1"
+                        ]
+                    }
+                }
+            ],
+            "components": [
+                {
+                    "provides": "modelService",
+                    "type": "provider",
+                    "implementation": ExampleStyleGuideModelProvider,
+                    "depends": [
+                        "$q"
+                    ]
+                }
+            ]
+        }
+    });
+});
